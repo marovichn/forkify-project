663d6c5f3c1b0942c31f428d115e4588926e3187
marovichn <nikolamarovic.11@gmail.com>
2023-02-06 22:41:10 +0100

    final

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -2,7 +2,6 @@ import View from './View.js';
 
 // icons
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
 
 class RecipeView extends View {
   _parentEl = document.querySelector('.recipe');
@@ -131,9 +130,7 @@ class RecipeView extends View {
                     <use href="${icons}#icon-check"></use>
                 </svg>
                 <div class="recipe__quantity">${
-                  ing.quantity
-                    ? /* new Fraction(ing.quantity).toString() */ ing.quantity
-                    : ''
+                  ing.quantity ? ing.quantity : ''
                 }</div>
                 <div class="recipe__description">
                     <span class="recipe__unit">${ing.unit}</span>
