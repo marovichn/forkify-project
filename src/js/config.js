7354b34c3d44230ab17dd5c5c20fe01498a1b9f6
marovichn <nikolamarovic.11@gmail.com>
2023-02-06 23:00:35 +0100

    final version

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 15;
 export const RES_PER_PAGE = 13;
-export const KEY = 'd908f9de-8247-4ddf-be89-6749e4980508';
+export const KEY = '48bb6e95-81e6-46e8-a03d-dd4ef2d57003';
 export const MODAL_CLOSE_SEC = 3;
--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -130,7 +130,7 @@ class RecipeView extends View {
                     <use href="${icons}#icon-check"></use>
                 </svg>
                 <div class="recipe__quantity">${
-                  ing.quantity ? ing.quantity : ''
+                  ing.quantity ? Math.round(ing.quantity * 100) / 100 : ''
                 }</div>
                 <div class="recipe__description">
                     <span class="recipe__unit">${ing.unit}</span>
